e199d49b8f837388d6b771a1213fac5bdbaeb59e
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-01 22:05:57 +0900

    add: デプロイのための設定・編集

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,9 @@ set -e
 # Rails に対応したファイル server.pid が存在しているかもしれないので削除する。
 rm -f /app/tmp/pids/server.pid
 
+RAILS_ENV=production bundle exec rails assets:precompile
+RAILS_ENV=production bundle exec rails assets:clean
+RAILS_ENV=production bundle exec rails db:migrate
+
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
 exec "$@"
