9592bd67f4158625e44a3cf364a4cfa8d81e08c1
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-03 19:29:45 +0900

    add: usersコントローラにコード記述、ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "tops#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :users, only: [:create, :new]
   # Defines the root path route ("/")
   # root "articles#index"
 end
