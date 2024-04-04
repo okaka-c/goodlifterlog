04812b97179a710fcd741c73ae888dcce13e11d7
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-04 12:04:58 +0900

    add: user_sessionsコントローラ雛形生成
    ログイン・ログアウト機能つくるため
    
    Issue #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user_sessions/new'
+  get 'user_sessions/create'
+  get 'user_sessions/destroy'
   root "tops#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new]
