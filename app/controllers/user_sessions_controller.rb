76874db8d7a4d37ad5f414738060e25ddac74d7f
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-04 15:07:13 +0900

    fix: ログイン成功後のリダイレクト先を大会結果一覧ページにした

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,7 +4,7 @@ class UserSessionsController < ApplicationController
   def create
     @user = login(params[:email], params[:password])
     if @user
-      redirect_to root_path
+      redirect_to competitions_path
     else
       render :new
     end
