ac422a654fe7aa6c3a3a730d7ad07e5717b8ffe8
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-04 12:19:40 +0900

    add: ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
   root "tops#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: [:create, :new]
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'login', to: 'user_sessions#new', :as => :login
+  post 'login', to: "user_sessions#create"
+  delete 'logout', to: 'user_sessions#destroy', :as => :logout
 end
