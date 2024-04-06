d104088e0a3ba2682e03ede40331e9b34653f106
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-06 19:19:10 +0900

    add: Competitionモデルにバリテーションの記述
    fearure #8

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,13 @@
 class Competition < ApplicationRecord
   belongs_to :user
+
+  validates: name, presence: true, length: { maximum: 255 }
+  validates: venue, length: { maximum: 255 }
+  validates: date, presence: true
+  validates: type, presence: true
+  validates: gearcategory_type, presence: true
+  validates: category, presence: true
+  validates: age_group, presence: true
+  validates: weight_class, presence: true
+  validates: participation_status, presence: true
 end
