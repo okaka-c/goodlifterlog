b7f53f9795fca6251783ffc8af150d2abb7f819b
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-07 18:14:29 +0900

    fix: validatesメソッドの構文エラーを修正
    feature #8

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,15 +1,15 @@
 class Competition < ApplicationRecord
   belongs_to :user
 
-  validates: name, presence: true, length: { maximum: 255 }
-  validates: venue, length: { maximum: 255 }
-  validates: date, presence: true
-  validates: competition_type, presence: true
-  validates: gearcategory_type, presence: true
-  validates: category, presence: true
-  validates: age_group, presence: true
-  validates: weight_class, presence: true
-  validates: participation_status, presence: true
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :venue, length: { maximum: 255 }
+  validates :date, presence: true
+  validates :competition_type, presence: true
+  validates :gearcategory_type, presence: true
+  validates :category, presence: true
+  validates :age_group, presence: true
+  validates :weight_class, presence: true
+  validates :participation_status, presence: true
 
   # enum定義
   enum competition_type: { official: 0, unofficial: 1 } # 大会種別：公式大会、非公式大会
