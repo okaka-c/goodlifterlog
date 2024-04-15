7af61f4640d5744a7fc3e43e76b7f449edcfa4a6
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-16 00:14:14 +0900

    Revert "add: 大会結果記録入力ページとの追加"
    This reverts commit 68044d885611e02021dc043c939efa5609e9c121.
    
    間違えたためリバーとします

--- app/controllers/competition_records_controller.rb
+++ app/controllers/competition_records_controller.rb
@@ -1,6 +1,2 @@
 class CompetitionRecordsController < ApplicationController
-def new
-  @competition_records = CompetitionRecord.new
-  @competition = Competition.find(params[:competition_id])
-end
 end
