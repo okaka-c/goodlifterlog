68044d885611e02021dc043c939efa5609e9c121
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-16 00:05:50 +0900

    add: 大会結果記録入力ページとの追加
    feature #23

--- app/controllers/competition_records_controller.rb
+++ app/controllers/competition_records_controller.rb
@@ -1,2 +1,6 @@
 class CompetitionRecordsController < ApplicationController
+def new
+  @competition_records = CompetitionRecord.new
+  @competition = Competition.find(params[:competition_id])
+end
 end
