65590276b972001b329cd1e2240f4968f7ee84a8
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-16 21:30:15 +0900

    fix: 試技判定結果のバリデーションを修正
    presence: trueを追記
    未選択を登録できないようにする
    
    validateメソッドの追記
    重量に数字を入力したら、未試行を選択できないようにした。
    成功か、失敗か入力漏れを防ぐため
    （成績の計算に関わってくるため)
    
    feature #23

--- app/models/competition_record.rb
+++ app/models/competition_record.rb
@@ -30,15 +30,87 @@ class CompetitionRecord < ApplicationRecord
   validates :deadlift_second_attempt, numericality: true, allow_nil: true
   validates :deadlift_third_attempt, numericality: true, allow_nil: true
   # スクワット試技判定結果のバリテーション設定
-  validates :squat_first_attempt_result, numericality: { greater_than: 0 }, if: -> { squat_first_attempt.present? && squat_first_attempt >= 0 }
-  validates :squat_second_attempt_result, numericality: { greater_than: 0 }, if: -> { squat_second_attempt.present? && squat_second_attempt >= 0 }
-  validates :squat_third_attempt_result, numericality: { greater_than: 0 }, if: -> { squat_third_attempt.present? && squat_third_attempt >= 0 }
+  # 空は禁止
+  validates :squat_first_attempt_result, presence: true
+  validates :squat_second_attempt_result, presence: true
+  validates :squat_third_attempt_result, presence: true
+  # 重量の入力があったら成功か、失敗か選択させる
+  validate :squat_first_attempt_is_not_be_not_attempted
+  validate :squat_second_attempt_is_not_be_not_attempted
+  validate :squat_third_attempt_is_not_be_not_attempted
   # ベンチプレス試技判定結果のバリテーション設定
-  validates :benchpress_first_attempt_result, numericality: { greater_than: 0 }, if: -> { benchpress_first_attempt.present? && benchpress_first_attempt >= 0 }
-  validates :benchpress_second_attempt_result, numericality: { greater_than: 0 }, if: -> { benchpress_second_attempt.present? && benchpress_second_attempt >= 0 }
-  validates :benchpress_third_attempt_result, numericality: { greater_than: 0 }, if: -> { benchpress_third_attempt.present? && benchpress_third_attempt >= 0 }
+  # 空は禁止
+  validates :benchpress_first_attempt_result, presence: true
+  validates :benchpress_second_attempt_result, presence: true
+  validates :benchpress_third_attempt_result, presence: true
+  # 重量の入力があったら成功か、失敗か選択させる
+  validate :benchpress_first_attempt_is_not_be_not_attempted
+  validate :benchpress_second_attempt_is_not_be_not_attempted
+  validate :benchpress_third_attempt_is_not_be_not_attempted
   # デッドリフト試技判定結果のバリテーション設定
-  validates :deadlift_first_attempt_result, numericality: { greater_than: 0 }, if: -> { deadlift_first_attempt.present? && deadlift_first_attempt >= 0 }
-  validates :deadlift_second_attempt_result, numericality: { greater_than: 0 }, if: -> { deadlift_second_attempt.present? && deadlift_second_attempt >= 0 }
-  validates :deadlift_third_attempt_result, numericality: { greater_than: 0 }, if: -> { deadlift_third_attempt.present? && deadlift_third_attempt >= 0 }
+  # 空は禁止
+  validates :deadlift_first_attempt_result, presence: true
+  validates :deadlift_second_attempt_result, presence: true
+  validates :deadlift_third_attempt_result, presence: true
+  # 重量の入力があったら成功か、失敗か選択させる
+  validate :deadlift_first_attempt_is_not_be_not_attempted
+  validate :deadlift_second_attempt_is_not_be_not_attempted
+  validate :deadlift_third_attempt_is_not_be_not_attempted
+
+  # カスタムバリデータ　スクワット判定結果
+  def squat_first_attempt_is_not_be_not_attempted
+    if squat_first_attempt.present? && squat_first_attempt >= 0 && squat_first_attempt_result == "not_attempted"
+      errors.add(:squat_first_attempt_result, "スクワットの第１試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  def squat_second_attempt_is_not_be_not_attempted
+    if squat_second_attempt.present? && squat_second_attempt >= 0 && squat_second_attempt_result == "not_attempted"
+      errors.add(:squat_second_attempt_result, "スクワットの第2試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  def squat_third_attempt_is_not_be_not_attempted
+    if squat_third_attempt.present? && squat_third_attempt >= 0 && squat_third_attempt_result == "not_attempted"
+      errors.add(:squat_third_attempt_result, "スクワットの第3試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  # カスタムバリデータ　ベンチプレス判定結果
+  def benchpress_first_attempt_is_not_be_not_attempted
+    if benchpress_first_attempt.present? && benchpress_first_attempt >= 0 && benchpress_first_attempt_result == "not_attempted"
+      errors.add(:benchpress_first_attempt_result, "スクワットの第１試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  def benchpress_second_attempt_is_not_be_not_attempted
+    if benchpress_second_attempt.present? && benchpress_second_attempt >= 0 && benchpress_second_attempt_result == "not_attempted"
+      errors.add(:benchpress_second_attempt_result, "スクワットの第2試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  def benchpress_third_attempt_is_not_be_not_attempted
+    if benchpress_third_attempt.present? && benchpress_third_attempt >= 0 && benchpress_third_attempt_result == "not_attempted"
+      errors.add(:benchpress_third_attempt_result, "スクワットの第3試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  # カスタムバリデータ　デッドリフト判定結果
+  def deadlift_first_attempt_is_not_be_not_attempted
+    if deadlift_first_attempt.present? && deadlift_first_attempt >= 0 && deadlift_first_attempt_result == "not_attempted"
+      errors.add(:deadlift_first_attempt_result, "スクワットの第１試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  def deadlift_second_attempt_is_not_be_not_attempted
+    if deadlift_second_attempt.present? && deadlift_second_attempt >= 0 && deadlift_second_attempt_result == "not_attempted"
+      errors.add(:deadlift_second_attempt_result, "スクワットの第2試技判定結果は成功か失敗かを選んでください")
+    end
+  end
+
+  def deadlift_third_attempt_is_not_be_not_attempted
+    if deadlift_third_attempt.present? && deadlift_third_attempt >= 0 && deadlift_third_attempt_result == "not_attempted"
+      errors.add(:deadlift_third_attempt_result, "スクワットの第3試技判定結果は成功か失敗かを選んでください")
+    end
+  end
 end
