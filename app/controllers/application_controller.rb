15f22fca636f500a866dc2710ba24f1d9194ce23
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-04-27 22:24:40 +0900

    add: ボトムナビゲーションを大会情報記録時と、編集時のページは非表示にした
    スマホ操作だと、下にあることで誤操作をしてしまい
    画面遷移してしまう恐れがあるので
    
    feature #83

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :require_login
+  before_action :set_bottom_navi
 
   private
 
   def not_authenticated
     redirect_to login_path
+  end
+
+  def set_bottom_navi
+	  @show_bottom_nav = true
   end
 end
