66948fb371371d9fd917dca3af9f1a5070ff7283
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-05-22 19:51:48 +0900

    fix: Record::WeighInモデルの、attr_accessorにcompetition_idを追加
    paramsでおくられてきたcompetition_idも、sessionに保存しておくため。
    
    feature #34

--- app/models/record/weigh_in.rb
+++ app/models/record/weigh_in.rb
@@ -1,5 +1,15 @@
 class Record::WeighIn
   include ActiveModel::Model
-  attr_accessor :weight
+  attr_accessor :weight, :competition_id
+
   validates :weight, presence: true, numericality: true
+  validate :unique_competition_id
+
+  private
+
+  def unique_competition_id
+    if CompetitionRecord.exists?(competition_id: competition_id)
+      errors.add(:competition_id, "試技結果はすでに存在しています")
+    end
+  end
 end
