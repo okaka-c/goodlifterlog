902bc816cbd4041637677bc99087ed149675009a
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-06-01 13:24:07 +0900

    add: スクワットの試技結果編集機能を追加
    feature #157

--- app/controllers/record/squats_controller.rb
+++ app/controllers/record/squats_controller.rb
@@ -24,6 +24,44 @@ skip_before_action :set_bottom_navi, only: %i[ new edit ]
     end
   end
 
+  def edit
+    # competitionのレコードを取得してくる
+    @competition = current_user.competitions.find(params[:competition_id])
+    # 取得したcompetitionのレコードに紐づくcompetition_recordレコードを取得
+    @competition_record = @competition.competition_record
+    @squat = Record::Squat.new(
+		squat_first_attempt: @competition_record.squat_first_attempt,
+    squat_second_attempt: @competition_record.squat_second_attempt,
+    squat_third_attempt: @competition_record.squat_third_attempt,
+    squat_first_attempt_result: @competition_record.squat_first_attempt_result,
+    squat_second_attempt_result: @competition_record.squat_second_attempt_result,
+    squat_third_attempt_result: @competition_record.squat_third_attempt_result
+  )
+	end
+
+  def update
+    # ユーザーが入力した値を取得
+    @squat = Record::Squat.new(squat_params)
+    # competitionのレコードを取得してくる
+    @competition = current_user.competitions.find(params[:competition_id])
+    # 取得したcompetitionのレコードに紐づくcompetition_recordレコードを取得
+    @competition_record = @competition.competition_record
+    squat_update_params = {
+      squat_first_attempt: @squat.squat_first_attempt,
+      squat_second_attempt: @squat.squat_second_attempt,
+      squat_third_attempt: @squat.squat_third_attempt,
+      squat_first_attempt_result: @squat.squat_first_attempt_result,
+      squat_second_attempt_result: @squat.squat_second_attempt_result,
+      squat_third_attempt_result: @squat.squat_third_attempt_result
+    }
+    # 取得したレコードの値を、ユーザーが入力してきた値に上書きしてupdateする
+    if @competition_record.update(squat_update_params)
+      redirect_to competition_path(@competition) # 成功したら詳細ページへ遷移する
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def squat_params
@@ -33,3 +71,4 @@ skip_before_action :set_bottom_navi, only: %i[ new edit ]
     :squat_third_attempt, :squat_third_attempt_result)
   end
 end
+
