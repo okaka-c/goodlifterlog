6d239814b03bad87bf8aabc3cbf8e9f8cccd64e0
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-06-05 19:43:01 +0900

    add: 属性が数値であることを検証、nillを許容するバリデーション追加
    以下の場面では計算結果がnillになるため許容する
    シングルベンチプレスでは、SQやDLはnillになる
    失格となった場合もnillになる
    
    feature #149

--- app/models/competition_result.rb
+++ app/models/competition_result.rb
@@ -1,5 +1,13 @@
 class CompetitionResult < ApplicationRecord
   belongs_to :competition_record
 
-  validates :competition_record_id, uniqueness: true
+  validates :competition_record_id, uniqueness: tru
+  # 各計算値のバリデーション定義
+  with_options numericality: true, allow_nil: true do
+    validates :best_squat_weight
+    validates :best_benchpress_weight
+    validates :best_deadlift_weight
+    validates :total_lifted_weight
+    validates :ipf_points
+  end
 end
