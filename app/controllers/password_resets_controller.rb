ada0984d1d651bf21086bcbbdbdeb615ad33b722
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-06-12 00:22:40 +0900

    add: パスワードリセットのフラッシュメッセ
    feature #193

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -8,7 +8,7 @@ class PasswordResetsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     @user&.deliver_reset_password_instructions!
-    redirect_to login_path
+    redirect_to login_path, success: t('.success')
   end
 
   # パスワード再設定フォーム。受信したメールのリンクから実行される
@@ -30,7 +30,7 @@ class PasswordResetsController < ApplicationController
 
     @user.password_confirmation = params[:user][:password_confirmation]
     if @user.change_password(params[:user][:password])
-      redirect_to login_path
+      redirect_to login_path, success: t('.success')
     else
       render :edit, status: :unprocessable_entity
     end
