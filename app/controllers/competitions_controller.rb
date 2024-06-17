a052510c68ba9116bbda50238664a6e9de9c0358
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-06-17 20:55:04 +0900

    add: 比較につかった過去大会へのリンク
    feature #174

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -23,8 +23,8 @@ class CompetitionsController < ApplicationController
   end
 
   def show
-    past_competition = current_user.competitions.past_competitions(@competition.gearcategory_type, @competition.category).order(date: :desc).offset(1).first
-    @past_competition_result = past_competition.competition_result if past_competition.present?
+    @past_competition = current_user.competitions.past_competitions(@competition.gearcategory_type, @competition.category, @competition.date ).order(date: :desc).first
+    @past_competition_result = @past_competition.competition_result if @past_competition.present?
   end
 
   def edit; end
