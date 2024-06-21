ee64a5cc8343776a3c7d63d0baa78c59498ccd28
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-06-21 19:19:26 +0900

    add: 元に戻す

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
 
-  config.hosts << 'www.powerlifterslog.com'
+  # config.hosts << 'www.powerlifterslog.com'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
