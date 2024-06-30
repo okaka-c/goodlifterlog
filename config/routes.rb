d5963b84b479325f64ce296252c1f24fd2700e1e
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-06-30 16:05:12 +0900

    add: LINEBOTからのPOSTリクエスト用ルーティング
    feature #15

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
   get "oauth/callback", to: "oauths#callback"
   post "oauth/callback", to: "oauths#callback"
   get 'oauth/:provider', to: 'oauths#oauth', :as => :auth_at_provider
+  # LINE messaging APIのHTTP POSTリクエスト受信用
+  post "/callback", to: "line_bot#callback"
   # 利用規約とプライバシーポリシー
   get 'terms', to: 'static_pages#terms'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
