9ac178d8c204917e4d354dd2158b004c7a61defd
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-07-14 17:22:36 +0900

    add: 共通のクエリをまとめたQueryBuilderクラスを作成
    feature #13

---
+++ app/queries/query_builder.rb
@@ -0,0 +1,29 @@
+class QueryBuilder
+  def initialize(user)
+    @user = user
+    @relation = user.competitions.joins(:competition_record => :competition_result).where(participation_status: 0).order(date: :asc)
+  end
+
+	def with_competition_type(competition_type)
+		@relation = @relation.where(competition_type: competition_type)
+    self
+	end
+
+  def with_gearcategory_type(gearcategory_type)
+    @relation = @relation.where(gearcategory_type: gearcategory_type)
+    self
+  end
+
+  def with_category(category)
+    @relation = @relation.where(category: category)
+    self
+  end
+
+  def pluck_values(column)
+    @relation.pluck("competitions.date", column)
+  end
+
+  private
+
+  attr_reader :relation
+end
