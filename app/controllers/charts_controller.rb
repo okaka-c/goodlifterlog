076da770e4108a8e256bd3673b4e9462425f002f
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-07-18 21:14:10 +0900

    add: charts_controllerのindexアクションにて、Queryクラスからデータを抽出
    feature #13

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,4 +1,9 @@
 class ChartsController < ApplicationController
   def index
+    @user = current_user
+    @classic_power_total_weights = view_context.format_dates_and_weights(ClassicPowerliftingTotalLiftedWeightQuery.call(@user))
+    @classic_single_benchpress_weights = view_context.format_dates_and_weights(ClassicSingleBenchPressLiftedWeightQuery.call(@user))
+    @equipped_power_total_weights = view_context.format_dates_and_weights(EquippedPowerliftingTotalLiftedWeightQuery.call(@user))
+    @equipped_single_benchpress_weights = view_context.format_dates_and_weights(EquippedSingleBenchPressLiftedWeightQuery.call(@user))
   end
 end
