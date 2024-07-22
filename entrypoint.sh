901cbdab45201ed4bfc888ddf0c87755b5310232
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-07-22 23:44:41 +0900

    add: デプロイ時にシードファイルの実行をさせる
    feature #291

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ rm -f /app/tmp/pids/server.pid
 RAILS_ENV=production bundle exec rails assets:precompile
 RAILS_ENV=production bundle exec rails assets:clean
 RAILS_ENV=production bundle exec rails db:migrate
+RAILS_ENV=production bundle exec rails db:seed
 
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
