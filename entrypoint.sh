c235fd1fcd1ce59a32ec5f29b296b9950643ec6a
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-07-23 19:42:17 +0900

    fix: seedsファイル削除、entrypoint.shのdb:seedコマンド削除
    feature #291

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ rm -f /app/tmp/pids/server.pid
 RAILS_ENV=production bundle exec rails assets:precompile
 RAILS_ENV=production bundle exec rails assets:clean
 RAILS_ENV=production bundle exec rails db:migrate
-RAILS_ENV=production bundle exec rails db:seed
 
 
 # コンテナーのプロセスを実行する。（Dockerfile 内の CMD に設定されているもの。）
