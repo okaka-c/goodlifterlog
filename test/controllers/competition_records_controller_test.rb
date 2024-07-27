e3613d54f0f5aac9d39d0501e6945cf573c2efe0
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-07-27 17:38:06 +0900

    delete: コントローラ生成時に自動生成されたtestファイルは使わないので削除
    feature #247

--- test/controllers/competition_records_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CompetitionRecordsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/competitions_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class CompetitionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get competitions_index_url
-    assert_response :success
-  end
-end
