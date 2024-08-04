366dd71472c9a74349a871b9df9bcc26a88a2347
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-08-04 16:29:33 +0900

    add: describeブロック, contextブロック, itブロックを記載(中身は空)
    feature #248

--- spec/models/competition_spec.rb
+++ spec/models/competition_spec.rb
@@ -1,5 +1,60 @@
 require 'rails_helper'
 
 RSpec.describe Competition, type: :model do
+  describe 'バリデーションチェック' do
+    context '全てのフィールドが有効な場合' do
+      it '有効であること' do
+      end
+    end
 
+    context 'ユーザーidが空白のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '大会名が空白のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '大会名が225文字以上のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '施設名が225文字以上のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '開催日が空白のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '大会種別が空白のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context 'ギア種別が空白のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '競技種別が空白のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '年齢区分が未選択のとき' do
+      it '無効であること' do
+      end
+    end
+
+    context '階級区分が未選択のとき' do
+      it '無効であること' do
+      end
+    end
+  end
 end
