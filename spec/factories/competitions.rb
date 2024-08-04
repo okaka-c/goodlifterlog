459eb5811baf3e8a110662bfe35ef985bebf9185
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-08-04 16:11:19 +0900

    add: userとcompetitionのテストデータを定義
    feature #248

---
+++ spec/factories/competitions.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :competition do
+    name {"テスト大会"}
+    venue {"テスト会場"}
+    date { Date.new(2024, 6, 30) }
+    competition_type {"official"}
+    gearcategory_type {"raw"}
+    category {"パワーリフティング"}
+    age_group {"一般"}
+    weight_class {"女子47㎏級"}
+    participation_status {"participated"}
+    association :user
+  end
+end
