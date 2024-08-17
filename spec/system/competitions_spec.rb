dac268d62623ef459dfb27c31c534a0d89951467
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-08-17 20:57:52 +0900

    update: LINEログインの模擬が動作するかどうか確認用のテストコード記載
    feature #249

--- spec/system/competitions_spec.rb
+++ spec/system/competitions_spec.rb
@@ -1,9 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe "Competitions", type: :system do
+  let(:user) { create(:user) }
+
   before do
     driven_by(:rack_test)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  describe 'ログインができるかどうかテスト' do
+    context 'LINEログインをクリック' do
+      it 'ログイン処理が成功する' do
+        login(user)
+        expect(page).to have_content 'LINEログインしました'
+        expect(current_path).to eq competitions_path
+      end
+    end
+  end
+
+  # describe 'ログイン前' do
+  # end
+
+  # describe 'ログイン後' do
+  #   before { login(user) }
+  # end
 end
