69fedcc942debe5ee1130f18dbf982dbe69d84b8
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-08-17 20:53:45 +0900

    fix: FactoryBot、userとprofileの修正
    userが作成されたときにコールバック after(:create)を用いて
    profileのデータが作成されるように変更
    
    feature #249

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :profile do
-    user
     gender {"man"}
     birthday { Date.new(1993, 6, 30) }
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,9 @@ FactoryBot.define do
     password {'12345678'}
     password_confirmation {'12345678'}
     role {"user"}
-    association :profile
+
+    after(:create) do |user|
+      create(:profile, user: user)
+    end
   end
 end
