bc3435b26d4e53d481d67fdcd9fa1585bc686be1
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-08-18 13:43:14 +0900

    update: 各試験工程のブロックのみを用意した
    feature #249

--- spec/system/competitions_spec.rb
+++ spec/system/competitions_spec.rb
@@ -17,10 +17,42 @@ RSpec.describe "Competitions", type: :system do
     end
   end
 
-  # describe 'ログイン前' do
-  # end
-
-  # describe 'ログイン後' do
-  #   before { login(user) }
-  # end
+  describe '大会情報のCRUD機能'
+    describe '大会情報一覧' do
+      context 'ログインしていない場合' do
+        it 'ログイン前トップページにリダイレクトされること' do
+        end
+      end
+      context 'ログインしている場合' do
+      end
+    end
+    describe '大会情報 新規登録ページ' do
+      context 'ログインしていない場合' do
+        it 'ログイン前トップページにリダイレクトされること' do
+        end
+      end
+      context 'ログインしている場合' do
+      end
+    end
+    describe '大会情報 詳細ページ' do
+      context 'ログインしていない場合' do
+        it 'ログイン前トップページにリダイレクトされること' do
+        end
+      end
+      context 'ログインしている場合' do
+      end
+    end
+    describe '大会情報 更新ページ' do
+      context 'ログインしていない場合' do
+        it 'ログイン前トップページにリダイレクトされること' do
+        end
+      end
+      context 'ログインしている場合' do
+      end
+    end
+    describe '大会情報 削除ページ' do
+      context 'ログインしている場合' do
+      end
+    end
+  end
 end
