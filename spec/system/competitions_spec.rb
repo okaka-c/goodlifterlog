68b0fe4678065d75d1c5215bcdb708019b70425c
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-08-20 20:32:33 +0900

    update: ログインした場合のテストケースを記載
    feature #249

--- spec/system/competitions_spec.rb
+++ spec/system/competitions_spec.rb
@@ -26,9 +26,22 @@ RSpec.describe "Competitions", type: :system do
           expect(page).to have_content('ログインしてください'), 'フラッシュメッセージ「ログインしてください」が表示されていません'
         end
       end
+
       context 'ログインしている場合' do
+        it '正しいタイトルが表示されていること' do
+          # titleタグの中身を検証
+        end
+        context '大会情報が1件もない場合' do
+          it '何もない旨のメッセージが表示されること' do
+          end
+        end
+        context '大会情報がある場合' do
+          it '大会情報の一覧が表示されること' do
+          end
+        end
       end
     end
+
     describe '大会情報 新規登録ページ' do
       context 'ログインしていない場合' do
         it 'ログイン前トップページにリダイレクトされること' do
@@ -37,9 +50,20 @@ RSpec.describe "Competitions", type: :system do
           expect(page).to have_content('ログインしてください'), 'フラッシュメッセージ「ログインしてください」が表示されていません'
         end
       end
+
       context 'ログインしている場合' do
+        it '正しいタイトルが表示されていること' do
+          # titleタグの中身を検証
+        end
+
+        it '大会情報が作成できること' do
+        end
+
+        it '大会情報の作成に失敗すること' do
+        end
       end
     end
+
     describe '大会情報 詳細ページ' do
       context 'ログインしていない場合' do
         it 'ログイン前トップページにリダイレクトされること' do
@@ -48,9 +72,16 @@ RSpec.describe "Competitions", type: :system do
           expect(page).to have_content('ログインしてください'), 'フラッシュメッセージ「ログインしてください」が表示されていません'
         end
       end
+
       context 'ログインしている場合' do
+        it '大会情報の詳細が表示されること' do
+        end
+
+        it '正しいタイトルが表示されていること' do
+        end
       end
     end
+
     describe '大会情報 更新ページ' do
       context 'ログインしていない場合' do
         it 'ログイン前トップページにリダイレクトされること' do
@@ -59,11 +90,20 @@ RSpec.describe "Competitions", type: :system do
           expect(page).to have_content('ログインしてください'), 'フラッシュメッセージ「ログインしてください」が表示されていません'
         end
       end
+
       context 'ログインしている場合' do
+        it '大会情報が更新できること' do
+        end
+
+        it '大会情報の更新に失敗すること' do
+        end
       end
     end
+
     describe '大会情報 削除ページ' do
       context 'ログインしている場合' do
+        it '大会情報が削除できること' do
+        end
       end
     end
   end
