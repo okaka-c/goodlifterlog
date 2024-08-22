b4fd5a727565a9cbe87befa7451ba1267cc25363
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-08-22 23:47:19 +0900

    update: 大会情報一覧ページのログインしている状態のテストコード記載
    feature #249

--- spec/system/competitions_spec.rb
+++ spec/system/competitions_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Competitions", type: :system do
   let(:user) { create(:user) }
+  let(:another_user) { create(:user) }
   let(:competition) { create(:competition, user: user) }
   before do
     driven_by(:rack_test)
@@ -28,15 +29,51 @@ RSpec.describe "Competitions", type: :system do
       end
 
       context 'ログインしている場合' do
-        it '正しいタイトルが表示されていること' do
-          # titleタグの中身を検証
-        end
-        context '大会情報が1件もない場合' do
-          it '何もない旨のメッセージが表示されること' do
+        context "自分のアカウント" do
+          before do
+            login(user)
+          end
+
+          it '正しいタイトルが表示されていること' do
+            expect(current_path).to eq(competitions_path), "大会結果一覧ページにいません"
+            expect(page).to have_title("大会結果一覧 | PowerLifter's Log"), "大会一覧ページのタイトルに「大会結果一覧 | PowerLifter's Log」が含まれていません。"
+          end
+
+          context '大会情報が1件もない場合' do
+            it '何もない旨のメッセージが表示されること' do
+              expect(current_path).to eq(competitions_path), "大会結果一覧ページにいません"
+              expect(page).to have_content('出場済の大会情報がありません'), '大会情報が一件もない場合、「出場済の大会情報がありません」というメッセージが表示されていません'
+            end
+          end
+
+          context '大会情報がある場合' do
+            context '自分の大会情報' do
+              it '大会情報の一覧が表示されること' do
+                competition
+                visit '/competitions'
+                expect(page).to have_selector("#competition-id-#{competition.id}"), '大会結果が表示されていません'
+                expect(page).to have_content(competition.name), '大会名が表示されていません'
+                expect(page).to have_content(competition.competition_type_i18n), '公式大会or非公式大会が表示されていません'
+                expect(page).to have_content(competition.category), 'パワーリフティングorベンチプレスが表示されていません'
+                expect(page).to have_content(competition.date), '開催日が表示されていません'
+                expect(page).to have_content(competition.weight_class), '階級区分が表示されていません'
+                expect(page).to have_content(competition.gearcategory_type_i18n), 'ノーギアかフルギアか表示されていません'
+              end
+            end
           end
         end
-        context '大会情報がある場合' do
-          it '大会情報の一覧が表示されること' do
+
+        context "他人のアカウント" do
+          context '他人の大会情報' do
+            before do
+              login(another_user)
+            end
+
+            it '他人のユーザーの大会情報の一覧が表示されていないこと' do
+              competition
+              visit '/competitions'
+              expect(page).not_to have_selector("#competition-id-#{competition.id}"), '他人の大会情報が表示されています'
+            end
           end
         end
       end
