f73cbc5c728f16bbce5329b4bd4f93507d7cdfe2
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-02 21:14:42 +0900

    update: 自動修正(-a)実行
    feature #327

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,27 +1,20 @@
 class PasswordResetsController < ApplicationController
   skip_before_action :require_login
 
-  # PWリセット申請ページを表示する
   def new; end
 
-  # ユーザーがパスワードリセットフォームにメールアドレスを入力して送信したときに実行
+  def edit
+    @token = params[:id]
+    @user = User.load_from_reset_password_token(@token)
+    not_authenticated if @user.blank?
+  end
+
   def create
     @user = User.find_by(email: params[:email])
     @user&.deliver_reset_password_instructions!
     redirect_to login_path, success: t('.success')
   end
 
-  # パスワード再設定フォーム。受信したメールのリンクから実行される
-  # トークンでユーザーを検索し、有効期限もチェック
-  # トークンが見つかり, 有効であればそのユーザーを返す
-  def edit
-    @token = params[:id]
-    @user = User.load_from_reset_password_token(@token)
-    not_authenticated if @user.blank?
-    # 認証されていないユーザーを拒否する root_pathに返す
-  end
-
-  #ユーザーがパスワードリセットフォームを送信したときに実行されます。
   def update
     @token = params[:id]
     @user = User.load_from_reset_password_token(@token)
