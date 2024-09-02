46055218b7ef5455c9114530498d7cd9db1c2f3f
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-02 20:27:44 +0900

    update: charts_controller.rbのコード修正をした
    Metrics/MethodLength, Layout/LineLength対応
    
    feature #327

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,9 +1,35 @@
 class ChartsController < ApplicationController
   def index
     @user = current_user
-    @classic_power_total_weights = view_context.format_dates_and_weights(ClassicPowerliftingTotalLiftedWeightQuery.call(@user))
-    @classic_single_benchpress_weights = view_context.format_dates_and_weights(ClassicSingleBenchPressLiftedWeightQuery.call(@user))
-    @equipped_power_total_weights = view_context.format_dates_and_weights(EquippedPowerliftingTotalLiftedWeightQuery.call(@user))
-    @equipped_single_benchpress_weights = view_context.format_dates_and_weights(EquippedSingleBenchPressLiftedWeightQuery.call(@user))
+    @classic_power_total_weights = fetch_classic_power_total_weights
+    @classic_single_benchpress_weights = fetch_classic_single_benchpress_weights
+    @equipped_power_total_weights = fetch_equipped_power_total_weights
+    @equipped_single_benchpress_weights = fetch_equipped_single_benchpress_weights
+  end
+
+  private
+
+  def fetch_classic_power_total_weights
+    view_context.format_dates_and_weights(
+      ClassicPowerliftingTotalLiftedWeightQuery.call(@user)
+    )
+  end
+
+  def fetch_classic_single_benchpress_weights
+    view_context.format_dates_and_weights(
+      ClassicSingleBenchPressLiftedWeightQuery.call(@user)
+    )
+  end
+
+  def fetch_equipped_power_total_weights
+    view_context.format_dates_and_weights(
+      EquippedPowerliftingTotalLiftedWeightQuery.call(@user)
+    )
+  end
+
+  def fetch_equipped_single_benchpress_weights
+    view_context.format_dates_and_weights(
+      EquippedSingleBenchPressLiftedWeightQuery.call(@user)
+    )
   end
 end
