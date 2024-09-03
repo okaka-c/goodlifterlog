2ecf478f495ae6233b40175913ca7d1a827e15b2
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-03 20:27:43 +0900

    update: 自動修正(-a)実行と、一部エラー無効化
    テストコード書いてからリファクタリングをする
    
    feature #328

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,20 @@
 module ApplicationHelper
   def flash_background_color(type)
     case type.to_sym
-    when :success then "bg-success text-success-content"
-    when :danger  then "bg-error text-error-content"
-    when :warning  then "bg-warning text-warning-content"
-    else "bg-gray-500 text-black"
+    when :success then 'bg-success text-success-content'
+    when :danger  then 'bg-error text-error-content'
+    when :warning then 'bg-warning text-warning-content'
+    else 'bg-gray-500 text-black'
     end
   end
 
+  # rubocop:disable Metrics/MethodLength
   def default_meta_tags
     {
       og: {
         title: "PowerLifter's Log",
         description: 'パワーリフティング/シングルベンチプレス大会の大会結果を記録・管理！',
-        image: image_url('ogp.png'),
+        image: image_url('ogp.png')
       },
       twitter: {
         card: 'summary_large_image',
@@ -21,4 +22,5 @@ module ApplicationHelper
       }
     }
   end
+  # rubocop:enable Metrics/MethodLength
 end
