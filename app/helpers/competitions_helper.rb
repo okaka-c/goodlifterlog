6310cb5c85e65c2c805a23f77626f223c5f13cbf
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-03 20:47:52 +0900

    update: case文からif-elsif構文に表現を変更
    条件が「真か偽か」で判断される状況では、if-elsif文が適しているため
    
    featurer #328

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -4,24 +4,28 @@ module CompetitionsHelper
   end
 
   def subtraction_result_color(number)
-    case true
-    when number.positive? then 'bg-red-50 text-red-600'
-    when number.negative? then 'bg-blue-50 text-blue-600'
-    when number.zero? then 'bg-orange-50 text-orange-600'
+    if number.positive?
+      'bg-red-50 text-red-600'
+    elsif number.negative?
+      'bg-blue-50 text-blue-600'
+    elsif number.zero?
+      'bg-orange-50 text-orange-600'
     end
   end
 
   def subtraction_result_allow(number)
-    case true
-    when number.positive? then
+    if number.positive?
+
       '<svg class="w-3 h-3 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
         <path fill="#ef4444" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
       </svg>'
-    when number.negative? then
+    elsif number.negative?
+
       '<svg class="w-3 h-3 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
         <path fill="#2563eb" d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"/>
       </svg>'
-    when number.zero? then
+    elsif number.zero?
+
       '<svg class="w-3 h-3 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
         <path fill="#ea580c" d="M224 32c0-17.7-14.3-32-32-32s-32 14.3-32 32V144H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H160V320c0 17.7 14.3 32 32 32s32-14.3 32-32V208H336c17.7 0 32-14.3 32-32s-14.3-32-32-32H224V32zM0 480c0 17.7 14.3 32 32 32H352c17.7 0 32-14.3 32-32s-14.3-32-32-32H32c-17.7 0-32 14.3-32 32z"/>
       </svg>'
