7db665f49c5f8389a916a1d63ec3d767364c304b
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-03 20:42:23 +0900

    update: Style/EmptyCaseConditionの対応
    caseに条件文を記載していないので、trueをつけた
    
    feature #328

--- app/helpers/competitions_helper.rb
+++ app/helpers/competitions_helper.rb
@@ -4,15 +4,15 @@ module CompetitionsHelper
   end
 
   def subtraction_result_color(number)
-    case
-    when number.positive? then "bg-red-50 text-red-600"
-    when number.negative? then "bg-blue-50 text-blue-600"
-    when number.zero? then "bg-orange-50 text-orange-600"
+    case true
+    when number.positive? then 'bg-red-50 text-red-600'
+    when number.negative? then 'bg-blue-50 text-blue-600'
+    when number.zero? then 'bg-orange-50 text-orange-600'
     end
   end
 
   def subtraction_result_allow(number)
-    case
+    case true
     when number.positive? then
       '<svg class="w-3 h-3 inline-block" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
         <path fill="#ef4444" d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"/>
