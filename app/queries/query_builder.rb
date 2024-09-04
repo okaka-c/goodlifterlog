7cdabe7bf225a8f3a6e67382245bf9955e4dcd8f
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-04 19:29:57 +0900

    update: Layout/LineLengthの対応
    Layout/LineLength: Line is too long.
    
    feature #329

--- app/queries/query_builder.rb
+++ app/queries/query_builder.rb
@@ -1,7 +1,10 @@
 class QueryBuilder
   def initialize(user)
     @user = user
-    @relation = user.competitions.joins(competition_record: :competition_result).where(participation_status: 0).order(date: :asc)
+    @relation = user.competitions
+                    .joins(competition_record: :competition_result)
+                    .where(participation_status: 0)
+                    .order(date: :asc)
   end
 
   def with_competition_type(competition_type)
