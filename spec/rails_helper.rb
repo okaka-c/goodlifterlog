079760e6b8c159263988c41b93e9acd4dc3231c4
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-16 17:24:53 +0900

    fix: chromeコンテナの設定をcapybara.rbに集約するため削除した
    feature #331

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -64,16 +64,4 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
   config.include LoginMacros
-
-  Capybara.register_driver :selenium_chrome_headless do |app|
-    options = Selenium::WebDriver::Chrome::Options.new
-    options.add_argument('--headless')
-    options.add_argument('--disable-gpu') # 古いバージョンのChrome用
-    options.add_argument('--no-sandbox') # CI環境での実行に必須
-    options.add_argument('--window-size=1920,1080')
-
-    Capybara::Selenium::Driver.new(app, browser: :chrome, options:)
-  end
-
-  Capybara.javascript_driver = :selenium_chrome_headless
 end
