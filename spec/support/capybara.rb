02e200c3d46588c208e98a5ad1a46bfea541cad5
imaisachiyo <imaisachiyo.1030@gmail.com>
2024-09-16 19:55:48 +0900

    add: 異なる環境でのシステムテストを考慮したCapybaraの設定を追加
    CI環境では、リモートのChromeインスタンスに接続するremote_chromeを使用。
    ローカル環境では、headless_chromeを引き続き使用し、ヘッドレスモードでのテストを行います。
    
    feature #331

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -11,15 +11,15 @@ end
 RSpec.configure do |config|
   if ENV['CODEBUILD_BUILD_ID']
     config.before(:each, type: :system) do
-      driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080]
-    end
-  else
-    config.before(:each, type: :system) do
       driven_by :remote_chrome
       Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
       Capybara.server_port = 4444
       Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
       Capybara.ignore_hidden_elements = false
+    end
+  else
+    config.before(:each, type: :system) do
+      driven_by :selenium, using: :headless_chrome, screen_size: [1920, 1080]
     end
   end
 end
